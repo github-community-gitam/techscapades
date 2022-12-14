741c76fb201567fc526fddd4bc84b0b5b66b9be2
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-12-14 14:56:03 +0530

    added auth guard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,11 +15,9 @@ export class LoginComponent implements OnInit {
     password: new FormControl('', Validators.required)
   })
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
 
   login() {
     if (!this.formData.valid) {
@@ -26,6 +25,9 @@ export class LoginComponent implements OnInit {
     } else {
       this.http.post('https://TreasureHunt.supersum4n.repl.co/login', this.formData.value).subscribe((res: any) => {
         if (res.status) {
+          localStorage.setItem('username', this.formData.controls.username.value as string)
+          localStorage.setItem('password', this.formData.controls.password.value as string)
+          this.router.navigate(['/dashboard'])
           alert('User authenticated successfully')
         } else {
           alert('User not found')
